145a247ec4868ac04e41fc94b97fa2a081d18325
Giacomo <giacomo.verdicchio@mail.polimi.it>
2023-11-30 16:37:57 +0100

    Added the feature of fast movement

--- src/main/java/files/controllerGUI/MainScreenController.java
+++ src/main/java/files/controllerGUI/MainScreenController.java
@@ -111,5 +111,10 @@ public class MainScreenController extends MainGUI {
         Stage stage = (Stage) exitButton.getScene().getWindow();
         stage.close();
     }
+
+
+
+    //try to add a new thing in the git
 }
+
 
